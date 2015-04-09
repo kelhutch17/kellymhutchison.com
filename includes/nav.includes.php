a00cb79f113a168ed1e1dd4c34ff82b65e69236c
Kelly Hutchison <kelhutch@verizon.net>
2015-04-08 23:37:19 -0400

    Initial commit - here's my website

---
+++ includes/nav.includes.php
@@ -0,0 +1,30 @@
+<!-- Begin navbar include -->
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Kelly M. Hutchison</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/files/Hutchison_PSU_cmpsc_2015_Resume.pdf">Resume</a></li>
+				<!--	<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul> --> 
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+	<!-- End navbar include -->
---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<head>
+	<?php include("includes/header.includes.php"); ?>
+	<title>Kelly M. Hutchison</title>
+</head>
+<body>
+	<!--[if lt IE 7]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+	<?php include("includes/nav.includes.php"); ?>
+
+	<!-- Main jumbotron for a primary marketing message or call to action -->
+	<div class="jumbotron splashkitten" data-stellar-background-ratio="0.5">
+		<div class="container">
+			<div class="col-md-6 splashtext">
+				<h1>Welcome!</h1>
+				<p>I’m Kelly, a senior at Penn State University majoring in Computer Science with an interest in iOS Development!</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<!-- Left Column Here -->
+			<div class="col-md-1">
+				<ul class="no-bullets">
+					<li><a href="mailto:kellyhutch17@gmail.com" class="email">kellyhutch17@gmail.com<span></span></a></li>
+					<li><a href="https://github.com/kelhutch17" class="github">GitHub<span></span></a></li>
+					<li><a href="https://www.linkedin.com/in/kelhutch17" class="linkedin">Linkedin<span></span></a></li>
+					<li><a href="https://twitter.com/kelhutch17" class="twitter">Twitter</a></li>
+					<li><a href="https://www.facebook.com/kelhutch17" class="facebook">Facebook<span></span></a></li>
+				</ul>
+			</div>
+
+			<!-- Center Column Here -->
+			<div class="col-md-7">
+				<div class="col-md-12">
+					<br>
+					<h2>Experience</h2>
+					<p><strong>Internships, Co-ops, and Jobs</strong><br>
+					<ul>
+					<li><strong>Rockwell Automation</strong>, Software Engineering Co-op (January 2013-August 2013)</li>
+					<li><strong>Rockwell Automation</strong>, Software Engineering Intern (May 2014-August 2014)</li>
+					<li><strong><a href="http://www.p212121.com">P212121</a></strong>, Software Developer </li>
+					<li><strong><a href="http://www.karat.io"> Karat </a></strong>, Technical Consultant </li>
+					</ul>
+					</p>
+
+				</div>
+				<div class="col-md-12">
+					<h2>Projects</h2>
+					<p>
+					<ul>
+						<li><strong><a href="https://github.com/kelhutch17/PictaPhone">PictaPhone</a></strong>, an iPhone application written in Obj C</li>
+						<li><strong><a href="https://github.com/kelhutch17/Drinkr">Drinkr</a></strong>, a webapp to track your BAC</li>
+						<li><strong>Raffle Tracking System, Penn State Dance Marathon</strong>
+							<ul>
+								<li>Transformed a manual ticket sales system into a web interface to track ticket sales</li>
+								<li>Created an automated raffle system to pick a winning ticket based on custom criteria</li>
+								<li>Created system using PHP, MySQL, and JavaScript</li>
+							</ul>
+						</li>
+						<li><strong>SMSA Device Driver</strong> written in C</li>
+						<li><strong>Custom Implementation of Malloc and Free</strong> in C</li>
+						<li><strong>AOPLister</strong>, a C# console application using the .NET framework</li>
+					</ul>
+					</p>
+				</div>
+				<div class="col-md-12">
+					<h2>Leadership &amp; Activities</h2>
+					<p>
+					<strong>
+					<ul>
+						<li> <a href="http://www.awc.cse.psu.edu">Association of Women in Computing</a> (AWC, Penn State) - Co-Founder (2012-present)</li>
+						<li><a href="http://acm.cse.psu.edu/"> Association for Computing Machinery</a>  (ACM, Penn State Chapter) - Member (2012-present)</li>
+						<li><a href="http://www.thon.org">Penn State Dance Marathon</a> </li>
+					</strong>
+						<ul>
+							<li><a href="http://www.thon.org/Students/GetInvolved/BeCaptain/Technology">Technology Captain</a> - THINK Developer (2014-2015)</li>
+							<li> Operations Committee Member (2013-2014) </li>
+							<li> Rules and Regulations Committee Member (2011-2012) </li>
+						</ul>
+					</ul>
+					</p>
+				</div>
+
+				<div class="col-md-12">
+					<h2>Coursework</h2>
+					<p><strong>Major Courses<br>
+					</strong></p>
+					<ul>
+						<li>CMPSC 475 - Applications Programming (iOS Development in Obj C) </li>
+						<li>CMPSC 483W – Software Design Methods (Senior Capstone)</li>
+						<li>CMPSC 473 – Operating Systems</li>
+						<li>CMPEN 331 – Computer Organization and Design</li>
+						<li>CMPEN 271 – Introduction to Digital Design</li>
+						<li>CMPSC 465 – Data Structures and Algorithms</li>
+						<li>CMPSC 461 – Programming Language Concepts</li>
+						<li>CMPSC 311 – Introduction to Systems Programming (C in Linux)</li>
+						<li>CMPSC 360 – Discrete Mathematics</li>
+						<li>CMPSC 221 – Object Oriented Programming with Web-based Applications (JAVA and MySQL)</li>
+						<li>CMPSC 122 – Intermediate Programming (C++)</li>
+						<li>CMPSC 121 – Introduction to Programming Techniques</li>
+						<li>CMPSC 111S – Logic for Computer Science</li>	
+					</ul>
+					<p><strong>Supporting Courses<br>
+					</strong></p>
+					<ul>
+						<li>IST 426 – Invention Commercialization</li>
+						<li>IST 441 – Information Retrieval</li>
+						<li>ENG 202C – Technical Writing</li>
+						<li>ENGR 297B – Special Topics (Women in Engineering Program Orientation)</li>
+						<li>ENGR 295A – Co-op Rotation 1</li>
+						<li>ENGR 395A – Co-op Rotation 2</li>
+						<li>MATH 140 – Calculus 1</li>
+						<li>MATH 141 – Calculus 2</li>
+						<li>MATH 220 – Matrices</li>
+						<li>MATH 230 – Calculus 3</li>
+						<li>STAT 318 – Elementary Probability</li>
+						<li>STAT 319 – Applied Statistics in Science</li>
+					</ul>
+				</div>
+			</div>
+
+			<!-- Right Column Here -->
+			<div class="col-md-4">
+				<p><img src="img/kelly.jpg"></p>
+				<h2>Kelly Hutchison</h2>
+				<p> <a href="/files/Hutchison_PSU_cmpsc_2015_Resume.pdf">Link to Resume</a> </p>
+			<!--	<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+			</div>
+		</div>	
+			<?php include("includes/footer.includes.php") ?>
+		</div> 
+<!-- /container -->
+</body>
+</html>
